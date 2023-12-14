7d3c4c458ed54ba8cfe28bbfe8fe03e3a61eda12
Tanuj Khanna <tanujkhanna72@gmail.com>
2023-12-14 23:09:08 +0530

    Setting up List Screen

---
+++ app/src/main/java/com/example/quotes/DataManager.kt
@@ -0,0 +1,25 @@
+package com.example.quotes
+
+import android.content.Context
+import androidx.compose.runtime.mutableStateOf
+import com.example.quotes.model.Quote
+import com.google.gson.Gson
+
+object DataManager {
+
+    var data = emptyArray<Quote>()
+    var isDataLoaded = mutableStateOf(false)
+
+    fun loadAssetsFromFile(context: Context) {
+        val inputStream = context.assets.open("quotes.json")
+        val size: Int = inputStream.available()
+        val buffer = ByteArray(size)
+        inputStream.read(buffer)
+        inputStream.close()
+        val json = String(buffer, Charsets.UTF_8)
+        val gson = Gson()
+        data = gson.fromJson(json, Array<Quote>::class.java)
+        isDataLoaded.value = true
+    }
+
+}
---
+++ app/src/main/java/com/example/quotes/model/Quote.kt
@@ -0,0 +1,6 @@
+package com.example.quotes.model
+
+data class Quote(
+    val text: String,
+    val author: String
+)
